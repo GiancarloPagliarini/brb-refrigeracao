f883d1ecc468574f3556b154191b484d068536b9
Giancarlo Pagliarini Alves <giancarlopalvess@outlook.com>
2024-11-03 19:26:56 -0300

    add: Creation budget session

--- brb-refrigeracao/src/components/Containers/Orcamentos/index.tsx
+++ brb-refrigeracao/src/components/Containers/Orcamentos/index.tsx
@@ -1,10 +1,51 @@
 const Orcamentos = () => {
-
     return (
-        <div>
-            <p className="text-center underline text-2xl font-medium">Como realizar um orçamento conosco?</p>
-            <div className="h-96">
+        <div className="w-full max-w-4xl mx-auto p-6">
+            <p className="text-center text-2xl font-medium mb-20">Como realizar um orçamento conosco?</p>
+            <div className="relative flex items-center justify-between">
+                <div className="absolute top-1/2 left-0 right-0 h-0.5 bg-cyan-200"></div>
 
+                <div className="relative flex flex-col items-center">
+                    <div className="bg-blue-300 text-blue-800 w-12 h-12 rounded-full flex items-center justify-center text-xl font-bold z-10">1.</div>
+                </div>
+
+                <div className="relative flex flex-col items-center">
+                    <div className="bg-blue-300 text-blue-800 w-12 h-12 rounded-full flex items-center justify-center text-xl font-bold z-10">2.</div>
+                </div>
+
+                <div className="relative flex flex-col items-center">
+                    <div className="bg-blue-300 text-blue-800 w-12 h-12 rounded-full flex items-center justify-center text-xl font-bold z-10">3.</div>
+                </div>
+
+                <div className="relative flex flex-col items-center">
+                    <div className="bg-blue-300 text-blue-800 w-12 h-12 rounded-full flex items-center justify-center text-xl font-bold z-10">4.</div>
+                </div>
+
+                <div className="relative flex flex-col items-center">
+                    <div className="bg-blue-300 text-blue-800 w-12 h-12 rounded-full flex items-center justify-center text-xl font-bold z-10">5.</div>
+                </div>
+
+            </div>
+            <div className="flex items-center justify-between">
+                <div className="">
+                    Passo 1
+                </div>
+
+                <div className="">
+                    Passo 2
+                </div>
+
+                <div className="">
+                    Passo 3
+                </div>
+
+                <div className="">
+                    Passo 4
+                </div>
+
+                <div className="">
+                    Passo 5
+                </div>
             </div>
         </div>
     )
--- brb-refrigeracao/src/components/HeaderHome/index.tsx
+++ brb-refrigeracao/src/components/HeaderHome/index.tsx
@@ -1,20 +1,20 @@
 const HeaderHome = () => {
     return (
-        <header className="p-10 bg-white">
+        <header className="px-8 py- bg-white">
             <div className="flex justify-around">
-                <h1 className="bg-blue-400 rounded p-3 inline-block text-white">Logo BRB</h1>
+                <img src="./images/logoAntiga.jpg" className="w-32 my-6" />
                 <ul className="flex items-center gap-2">
-                    <li className="relative p-1 pr-2 pl-2 rounded cursor-pointer text-black hover:text-blue-500 transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[1px] before:bg-black hover:before:w-full before:transition-all before:duration-300">
-                        Exemplo 1
+                    <li className="relative p-1 pr-2 pl-2 rounded cursor-pointer text-black hover:text-blue-500 transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[1px] before:bg-blue-500 hover:before:w-full before:transition-all before:duration-300">
+                        Orçamentos
                     </li>
-                    <li className="relative p-1 pr-2 pl-2 rounded cursor-pointer text-black hover:text-blue-500 transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[1px] before:bg-black hover:before:w-full before:transition-all before:duration-300">
-                        Exemplo 2
+                    <li className="relative p-1 pr-2 pl-2 rounded cursor-pointer text-black hover:text-blue-500 transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[1px] before:bg-blue-500 hover:before:w-full before:transition-all before:duration-300">
+                        O que nos diferencia?
                     </li>
-                    <li className="relative p-1 pr-2 pl-2 rounded cursor-pointer text-black hover:text-blue-500 transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[1px] before:bg-black hover:before:w-full before:transition-all before:duration-300">
-                        Exemplo 3
+                    <li className="relative p-1 pr-2 pl-2 rounded cursor-pointer text-black hover:text-blue-500 transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[1px] before:bg-blue-500 hover:before:w-full before:transition-all before:duration-300">
+                        Entre em contato
                     </li>
-                    <li className="relative p-1 pr-2 pl-2 rounded cursor-pointer text-black hover:text-blue-500 transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[1px] before:bg-black hover:before:w-full before:transition-all before:duration-300">
-                        Exemplo 4
+                    <li className="relative p-1 pr-2 pl-2 rounded cursor-pointer text-black hover:text-blue-500 transition-all duration-300 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[1px] before:bg-blue-500 hover:before:w-full before:transition-all before:duration-300">
+                        Sobre nós
                     </li>
                 </ul>
             </div>
